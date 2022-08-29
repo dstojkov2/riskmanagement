1cbe33d6534767296191524cae62e57629b4c8cb
dstojkov2 <dusan.stojkovic@sap.com>
2022-08-29 13:43:34 +0000

    minor change by Dusan, try 4

--- srv/risk-service.js
+++ srv/risk-service.js
@@ -14,7 +14,7 @@ module.exports = cds.service.impl(async function () {
    this.after("READ", Risks, (data) => {
       const risks = Array.isArray(data) ? data : [data];
 
-      console.log("Hello world...");
+      console.log("Hello world......");
       risks.forEach((risk) => {
          if (risk.impact >= 100000) {
             risk.criticality = 1;
